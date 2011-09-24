8dcfe7a887fc6a551f0afd2c144c1d9d63130ee9
Peter DeWeese <peter.deweese@eyestreet.com>
2011-09-23 23:16:02 -0400

    Added CG convenience methods.

--- es_ios_utils/universal/ESCGMethods.h
+++ es_ios_utils/universal/ESCGMethods.h
@@ -14,4 +14,34 @@
   +(CGPoint)centerOfSize:(CGSize)s;
   +(CGPoint)subtractPoint:(CGPoint)p from:(CGPoint)from;
   +(float)distanceFromOriginToPoint:(CGPoint)p;
+
+  //drawing methods. Methods without context references use the current context.
+  +(CGContextRef)currentContext;
+
+  +(void)beginPathInContext:(CGContextRef)c;
+  +(void)context:(CGContextRef)c drawPath:(CGPathDrawingMode)mode;
+
+  +(void)context:(CGContextRef)c setLineWidth:(CGFloat)w;
+  +(void)context:(CGContextRef)c setLineCap:(CGLineCap)cap;
+  +(void)context:(CGContextRef)c setStrokeColor:(UIColor*)color;
+  +(void)context:(CGContextRef)c setFillColor:(UIColor*)color;
+  +(void)context:(CGContextRef)c drawElipseInRect:(CGRect)r;
+  +(void)context:(CGContextRef)c drawCircleAt:(CGPoint)p radius:(float)r;
+  +(void)context:(CGContextRef)c moveTo:(CGPoint)p;
+  +(void)context:(CGContextRef)c addLineTo:(CGPoint)p;
+  +(void)context:(CGContextRef)c addLineFrom:(CGPoint)f to:(CGPoint)t;
+
+  +(void)beginPath;
+  +(void)drawPath:(CGPathDrawingMode)mode;
+  +(void)fillPath;
+
+  +(void)setLineWidth:(CGFloat)w;
+  +(void)setLineCap:(CGLineCap)cap;
+  +(void)setStrokeColor:(UIColor*)color;
+  +(void)setFillColor:(UIColor*)color;
+  +(void)drawElipseInRect:(CGRect)r;
+  +(void)drawCircleAt:(CGPoint)p radius:(float)r;
+  +(void)moveTo:(CGPoint)p;
+  +(void)addLineTo:(CGPoint)p;
+  +(void)addLineFrom:(CGPoint)f to:(CGPoint)t;
 @end
--- es_ios_utils/universal/ESCGMethods.m
+++ es_ios_utils/universal/ESCGMethods.m
@@ -43,4 +43,131 @@
     return sqrtf(p.x*p.x + p.y*p.y);
 }
 
+
+#pragma mark - Drawing Methods
+
++(CGContextRef)currentContext
+{
+    return UIGraphicsGetCurrentContext();
+}
+
++(void)beginPathInContext:(CGContextRef)c
+{
+    CGContextBeginPath(c);
+}
+
++(void)context:(CGContextRef)c drawPath:(CGPathDrawingMode)mode
+{
+    CGContextDrawPath(c, mode);
+}
+
++(void)context:(CGContextRef)c setLineWidth:(CGFloat)w
+{
+    CGContextSetLineWidth(c, w);
+}
+
++(void)context:(CGContextRef)c setLineCap:(CGLineCap)cap
+{
+    CGContextSetLineCap(c, cap);
+}
+
++(void)context:(CGContextRef)c setStrokeColor:(UIColor*)color
+{
+    CGContextSetStrokeColorWithColor(c, color.CGColor);
+}
+
++(void)context:(CGContextRef)c setFillColor:(UIColor*)color
+{
+    CGContextSetFillColorWithColor(c, color.CGColor);
+}
+
++(void)context:(CGContextRef)c drawElipseInRect:(CGRect)r
+{
+    CGContextAddEllipseInRect(c, r);
+}
+
++(void)context:(CGContextRef)c drawCircleAt:(CGPoint)p radius:(float)r
+{
+    [CG context:c drawElipseInRect:$rect(p.x - r, p.y-r, r*2, r*2)];
+}
+
++(void)context:(CGContextRef)c moveTo:(CGPoint)p
+{
+    CGContextMoveToPoint(c, p.x, p.y);
+}
+
++(void)context:(CGContextRef)c addLineTo:(CGPoint)p
+{
+    CGContextAddLineToPoint(c, p.x, p.y);
+}
+
++(void)context:(CGContextRef)c addLineFrom:(CGPoint)f to:(CGPoint)t
+{
+    [self context:c moveTo:f];
+    [self context:c addLineTo:t];
+}
+
+
+#pragma mark Use Current Context
+
++(void)beginPath
+{
+    [CG beginPathInContext:CG.currentContext];
+}
+
++(void)drawPath:(CGPathDrawingMode)mode
+{
+    [CG context:CG.currentContext drawPath:mode];
+}
+
++(void)fillPath
+{
+    [CG drawPath:kCGPathFillStroke];
+}
+
++(void)setLineWidth:(CGFloat)w
+{
+    [CG context:CG.currentContext setLineWidth:w];
+}
+
++(void)setLineCap:(CGLineCap)cap
+{
+    [CG context:CG.currentContext setLineCap:cap];
+}
+
++(void)setStrokeColor:(UIColor*)color
+{
+    [CG context:CG.currentContext setStrokeColor:color];
+}
+
++(void)setFillColor:(UIColor*)color
+{
+    [CG context:CG.currentContext setFillColor:color];
+}
+
++(void)drawElipseInRect:(CGRect)r
+{
+    [CG context:CG.currentContext drawElipseInRect:r];
+}
+
++(void)drawCircleAt:(CGPoint)p radius:(float)r
+{
+    [CG context:CG.currentContext drawCircleAt:p radius:r];
+}
+
++(void)moveTo:(CGPoint)p
+{
+    [CG context:CG.currentContext moveTo:p];
+}
+
++(void)addLineTo:(CGPoint)p
+{
+    [CG context:CG.currentContext addLineTo:p];
+}
+
++(void)addLineFrom:(CGPoint)f to:(CGPoint)t
+{
+    [CG context:CG.currentContext addLineFrom:f to:t];
+}
+
 @end
