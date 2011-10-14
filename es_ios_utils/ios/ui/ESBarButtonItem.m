a164236b6769cc081317bd8015fc4f243b6c616d
Peter DeWeese <peter.deweese@eyestreet.com>
2011-10-14 08:44:02 -0400

    Added retained objects cleanup method to rotatableViewController to reduce duplication. ESBarButtonItem can now create view controllers on the fly for each press or use a pre-defined view controller.

--- es_ios_utils/ios/ui/ESBarButtonItem.m
+++ es_ios_utils/ios/ui/ESBarButtonItem.m
@@ -30,21 +30,8 @@
     return item;
 }
 
-@synthesize blockAction, viewControllerForPopover, userTarget, userAction;
+@synthesize blockAction, createViewControllerForPopover, viewControllerForPopover, userTarget, userAction;
 @synthesize /*private*/ popoverController;
-
--(void)setViewControllerForPopover:(UIViewController*)new
-{
-    if(new != viewControllerForPopover)
-    {
-        [new retain];
-        [viewControllerForPopover release];
-        viewControllerForPopover = new;
-        
-        [popoverController dismissPopoverAnimated:YES];
-        self.popoverController = [UIPopoverController popoverControllerWithNavigationAndContentViewController:viewControllerForPopover];
-    }
-}
 
 //It's a bit of a hack, but target and action will not return what they are set to. This is how we coopt the press event while still allowing a user to set the target and action.
 -(id)target { return self; }
@@ -52,44 +39,59 @@
 -(SEL)action { return @selector(pressed:); }
 -(void)setAction:(SEL)new { self.userAction = new; }
 
--(void)presentPopover
-{
-    if(!popoverController.isPopoverVisible)
-    {
-        [popoverController presentPopoverFromBarButtonItem:self permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
-        [popoverController setPopoverContentSize:viewControllerForPopover.contentSizeForViewInPopover animated:NO];
-    }
-}
-
 -(void)pressed:(id)sender
 {
-    if(popoverController.isPopoverVisible)
+    if(popoverController)
         [self dismissPopover];
     else
     {
         if(userTarget && userAction && [userTarget respondsToSelector:userAction])
             [userTarget performSelector:userAction];
         
-        if(popoverController)
-            [self presentPopover];
+        [self presentPopover];
         
         if(blockAction) blockAction();
+    }
+}
+
+#pragma mark - Popovers
+
+-(void)presentPopover
+{
+    if(!popoverController)
+    {
+        UIViewController* vc = viewControllerForPopover ?: (createViewControllerForPopover!=nil ? createViewControllerForPopover() : nil);
+        if(vc)
+        {
+            self.popoverController = [UIPopoverController popoverControllerWithNavigationAndContentViewController:vc];
+            popoverController.delegate = self;
+            [popoverController presentPopoverFromBarButtonItem:self permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+        }
     }
 }
 
 -(void)dismissPopover
 {
     [popoverController dismissPopoverAnimated:YES];
+    self.popoverController = nil;
 }
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController*)pc
+{
+    self.popoverController = nil;
+}
+
+
+#pragma mark - Cleanup
 
 -(void)dealloc
 {
-    self.blockAction = nil;
-    self.viewControllerForPopover = nil;
-    self.popoverController = nil;
-    self.userTarget = nil;
-    self.userAction = nil;
-    
+    self.blockAction                    = nil;
+    self.viewControllerForPopover       = nil;
+    self.popoverController              = nil;
+    self.userTarget                     = nil;
+    self.userAction                     = nil;
+    self.createViewControllerForPopover = nil;
     [super dealloc];
 }
 
