11026a875d53233e2abd79121c2de37918babd41
Peter DeWeese <peter.deweese@eyestreet.com>
2012-03-13 23:57:40 -0400

    Added alpha indexing to array backed table view.

--- es_ios_utils/ios/ui/ESArrayBackedTableViewController.h
+++ es_ios_utils/ios/ui/ESArrayBackedTableViewController.h
@@ -5,10 +5,15 @@
   @property(retain) NSString* textKey;
   @property(retain) NSString* detailKey;
 
+  @property(readonly) BOOL usesSections;
+
+  -(void)convertToAlphaIndex;
+
   -(void)configureCellForData:(id)o;
 
   //Override these:
   -(NSArray*)cellData;
   -(void)configureCell:(UITableViewCell*)c withData:(id)o;
   -(void)didSelectCellWithData:(id)o;
+
 @end
--- es_ios_utils/ios/ui/ESArrayBackedTableViewController.m
+++ es_ios_utils/ios/ui/ESArrayBackedTableViewController.m
@@ -1,8 +1,56 @@
 #import "ESArrayBackedTableViewController.h"
 
+@interface ESArrayBackedTableViewController()
+  @property(retain) NSMutableArray* sectionTitles;
+  @property(retain) NSMutableArray* sectionData;
+@end
+
 @implementation ESArrayBackedTableViewController
 
 @synthesize cellData, textKey, detailKey;
+@synthesize sectionTitles, sectionData;
+
+-(id)initWithCoder:(NSCoder*)aDecoder
+{
+    if(self = [super initWithCoder:aDecoder])
+    {
+        sectionTitles = NSMutableArray.new;
+        sectionData   = NSMutableArray.new;
+    }
+    return self;
+}
+
+-(BOOL)usesSections
+{
+    return sectionData.isNotEmpty && [sectionData.first isKindOfClass:NSArray.class];
+}
+
+-(NSMutableArray*)sectionDataFor:(int)s
+{
+    return [sectionData objectAtIndex:s];
+}
+
+-(id)dataAtIndexPath:(NSIndexPath*)ip
+{
+    if(self.usesSections)
+        return [[self sectionDataFor:ip.section] objectAtIndex:ip.row];
+    return [cellData objectAtIndex:ip.row];
+}
+
+-(void)convertToAlphaIndex
+{
+    [cellData each:^(id o) {
+        NSString* text = [o valueForKey:textKey];
+        NSString* sectionTitle = [text substringToIndex:1];
+        if(![sectionTitle isEqualToString:sectionTitles.last])
+        {
+            [sectionTitles addObject:sectionTitle];
+            [sectionData addObject:NSMutableArray.new];
+        }
+        NSMutableArray* section = sectionData.last;
+        [section addObject:o];
+    }];
+}
 
 -(void)configureCellForData:(id)o
 {
@@ -19,21 +67,39 @@
 
 -(void)didSelectCellWithData:(id)o { }
 
+
 #pragma mark - Table view data source
 
--(int)numberOfSectionsInTableView:(UITableView*)tv { return 1; }
--(int)tableView:(UITableView*)tv numberOfRowsInSection:(int)s { return s==0 ? self.cellData.count : 0; }
+-(int)numberOfSectionsInTableView:(UITableView*)tv
+{
+    return self.usesSections ? sectionData.count : 1;
+}
+
+-(NSString*)tableView:(UITableView*)tv titleForHeaderInSection:(int)s
+{
+    return self.usesSections ? [sectionTitles objectAtIndex:s] : nil;
+}
+
+-(int)tableView:(UITableView*)tv numberOfRowsInSection:(int)s
+{
+    return self.usesSections ? [self sectionDataFor:s].count : cellData.count;
+}
 
 -(UITableViewCell*)tableView:(UITableView*)tv cellForRowAtIndexPath:(NSIndexPath*)ip
 {
     UITableViewCell *c = [tv dequeueReusableCellWithIdentifier:@"Cell"];
-    [self configureCell:c withData:[self.cellData objectAtIndex:ip.row]];
+    [self configureCell:c withData:[self dataAtIndexPath:ip]];
     return c;
 }
 
 -(void)tableView:(UITableView*)tv didSelectRowAtIndexPath:(NSIndexPath*)ip
 {
     [self didSelectCellWithData:[self.cellData objectAtIndex:ip.row]];
+}
+
+-(NSArray*)sectionIndexTitlesForTableView:(UITableView*)tv
+{
+    return self.usesSections ? sectionTitles : nil;
 }
 
 @end
